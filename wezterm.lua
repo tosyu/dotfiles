5500becbea88dc9cc47fbf9999d7b3e1a5a72c4a
Krzysztof Antoszek <tosyu@bytehog.pl>
2026-03-16 11:33:00 +0100

    yazi and wezterm configs

---
+++ wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require "wezterm"
+local config = wezterm.config_builder()
+
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+
+	return "Dark"
+end
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.color_scheme = "Modus-Vivendi"
+
+if get_appearance() == "Light" then
+	config.color_scheme = "Modus-Operandi"
+end
+
+return config
