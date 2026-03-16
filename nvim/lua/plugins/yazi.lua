6d62f10ddebdc18bac0da034696d4eb9a245a186
Krzysztof Antoszek <tosyu@bytehog.pl>
2026-03-16 11:07:41 +0100

    yazi

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,20 @@
+return {
+	"mikavilpas/yazi.nvim",
+	version = "*",
+	event = "VeryLazy",
+	dependencies = {"nvim-lua/plenary.nvim", lazy = true},
+	keys = {
+		{"<leader>-", mode = {"n", "v"}, "<cmd>Yazi<cr>", desc = "Open Yazi at the current file"},
+		{"<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Open Yazi at cwd"},
+		{"<leader>Y", "<cmd>Yazi toggle<cr>", desc = "Toggle Yazi"},
+	},
+	opts = {
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	init = function ()
+		vim.g.loaded_netrwPlugin = 1
+	end,
+}
